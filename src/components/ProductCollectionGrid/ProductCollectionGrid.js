67c54cd04cdbb30b5b91c53f394e719f3a76648b
QabiEj <101940223+QabiEj@users.noreply.github.com>
2023-12-15 13:57:23 +0100

    ndryshime teksti

--- src/components/ProductCollectionGrid/ProductCollectionGrid.js
+++ src/components/ProductCollectionGrid/ProductCollectionGrid.js
@@ -8,25 +8,25 @@ const ProductCollectionGrid = (props) => {
     <div className={styles.root}>
       <ProductCollection
         image={'/collections/collection1.png'}
-        title={'Men'}
+        title={'Meshkuj'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection2.png'}
-        title={'Women'}
+        title={'Femra'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection3.png'}
-        title={'Accessories'}
+        title={'Bizhuteri'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection4.png'}
-        title={'Simple Cotton'}
+        title={'Fëmijë'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
