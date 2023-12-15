738ead35cc22fd2b26219205af47cf272052d9fc
QabiEj <101940223+QabiEj@users.noreply.github.com>
2023-12-15 14:14:39 +0100

    ndryshime

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,7 +88,7 @@ const IndexPage = () => {
 
       {/* Promotion */}
       <div className={styles.promotionContainer}>
-        <Hero image={'/banner2.png'} title={`-50% off \n All Essentials`} />
+        <Hero image={'/banner2.png'} title={`-50% zbritje \n Në shumë produkte!`} />
         <div className={styles.linkContainers}>
           <Link to={'/shop'}>WOMAN</Link>
           <Link to={'/shop'}>MAN</Link>
