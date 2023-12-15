535741ebae6829e428d42757914136bff22dd529
QabiEj <101940223+QabiEj@users.noreply.github.com>
2023-12-15 11:00:34 +0100

    ndryshime faqja kryesore

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Shop online'}
+        subtitle={'Zbulo oferat e reja dimër 2023'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
